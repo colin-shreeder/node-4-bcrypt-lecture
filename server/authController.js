4103ea971b42a5fe9f5e7f4483da2539fe455d60
Adam Kent <adamkent@Adams-MacBook-Pro.local>
2020-11-10 11:08:31 -0700

    register

--- server/authController.js
+++ server/authController.js
@@ -1,6 +1,22 @@
-module.exports = {
-    register: (req, res) => {
+const bcrypt = require('bcrypt');
 
+module.exports = {
+    register: async (req, res) => {
+        const db = req.app.get('db');
+        const { email, username, password } = req.body;
+        const user = await db.check_user(email);
+        if(user[0]) {
+            return res.status(409).send("User already exists")
+        }
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        const [newUser] = await db.add_user([username, email, hash]);
+        req.session.user = {
+            userId: newUser.user_id,
+            email: newUser.email,
+            username: newUser.username
+        };
+        res.status(200).send(req.session.user);
     },
     login: (req, res) => {
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
+const session = require('express-session');
 // require session
 const auth = require('./authController');
-const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 const app = express();
 // Top level middleware
 app.use(express.json());
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
 
 massive({
     connectionString: CONNECTION_STRING,
@@ -20,5 +30,7 @@ massive({
 }).catch( err => console.log(err));
 
 // Enpoints
+app.post('/auth/register', auth.register);
+
 
 app.listen(SERVER_PORT, () => console.log(`Connected to port ${SERVER_PORT}⛵⚓`))
